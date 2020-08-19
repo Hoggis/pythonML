fa13007e3549aceb0cf6d442b2fb10f1a4f4778c
Hoggis <aki.hokkanen@gmail.com>
2020-08-19 19:52:36 +0300

    initial commit

--- Ecommerce with randomly generated data/Pandas ecommerce.py
+++ Ecommerce with randomly generated data/Pandas ecommerce.py
@@ -1,228 +1,62 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ___
-# 
-# <a href='http://www.pieriandata.com'> <img src='../../Pierian_Data_Logo.png' /></a>
-# ___
-# # Ecommerce Purchases Exercise
-# 
-# In this Exercise you will be given some Fake Data about some purchases done through Amazon! Just go ahead and follow the directions and try your best to answer the questions and complete the tasks. Feel free to reference the solutions. Most of the tasks can be solved in different ways. For the most part, the questions get progressively harder.
-# 
-# Please excuse anything that doesn't make "Real-World" sense in the dataframe, all the data is fake and made-up.
-# 
-# Also note that all of these questions can be answered with one line of code.
-# ____
-# ** Import pandas and read in the Ecommerce Purchases csv file and set it to a DataFrame called ecom. **
-
-# In[1]:
-
-
 import pandas as pd
 
-
-# In[2]:
-
+#The data for this excercise has been created at random.
+#The ideas for this data exploration are from a Udemy course.
+#More information from the README
 
 ecom = pd.read_csv('Ecommerce Purchases')
-
-
-# **Check the head of the DataFrame.**
-
-# In[3]:
-
-
 ecom.head(2)
 
-
-# ** How many rows and columns are there? **
-
-# In[4]:
-
-
+#Basic exploration
+#Rows
 ecom.info()
-
-
-# In[5]:
-
-
-#ecom.info()
-
-
-# ** What is the average Purchase Price? **
-
-# In[6]:
-
-
+# Mean purchase price
 ecom['Purchase Price'].mean()
-
-
-# In[7]:
-
-
-#mean()
-
-
-# ** What were the highest and lowest purchase prices? **
-
-# In[10]:
-
-
 #max
-
-
-# In[8]:
-
-
 ecom['Purchase Price'].max()
-
-
-# In[11]:
-
-
 #min
-
-
-# In[9]:
-
-
 ecom['Purchase Price'].min()
 
 
-# ** How many people have English 'en' as their Language of choice on the website? **
-
-# In[12]:
-
-
-ecom
-
-
-# In[16]:
-
-
+#Count of people who have selected english as their language
 ecom[ecom['Language']=='en'].count()
 
-
-# In[94]:
-
-
-
-
-
-# ** How many people have the job title of "Lawyer" ? **
-# 
-
-# In[18]:
-
-
+#Count of people who are lawyers
 ecom[ecom['Job']=='Lawyer'].count()
 
-
-# In[95]:
-
-
-
-
-
-# ** How many people made the purchase during the AM and how many people made the purchase during PM ? **
-# 
-# **(Hint: Check out [value_counts()](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.Series.value_counts.html) ) **
-
-# In[23]:
-
-
+#Check how many purchases were made AM and PM
 ecom['AM or PM'].value_counts()
 
-
-# ** What are the 5 most common Job Titles? **
-
-# In[26]:
-
-
+#Five most common job titles
 ecom['Job'].value_counts().head(5)
 
 
-# ** Someone made a purchase that came from Lot: "90 WT" , what was the Purchase Price for this transaction? **
-
-# In[28]:
-
-
+#Purchase price made from Lot "90 WT"
 ecom[ecom['Lot']=="90 WT"]['Purchase Price']
 
-
-# ** What is the email of the person with the following Credit Card Number: 4926535242672853 **
-
-# In[30]:
-
-
+#Email address of the person with CC num 4926535242672853
 ecom[ecom['Credit Card']== 4926535242672853]['Email']
 
-
-# ** How many people have American Express as their Credit Card Provider *and* made a purchase above $95 ?**
-
-# In[45]:
-
-
-ecom
-
-
-# In[47]:
-
-
+## CC Provider is American Express and Purchase made is over 95$
 sum(ecom[ecom['CC Provider']=='American Express']['Purchase Price']>95)
 
-
-# In[101]:
-
-
-
-
-
-# ** Hard: How many people have a credit card that expires in 2025? **
-
-# In[85]:
-
-
-#koska numerot on inttejä eikä stringejä niin ei toimi ilman ""
+#How many people have a credit card expiring 2025
 
 def exp2025(ccexp):
+    #25 has to be in "" or it will be int and slice wont work
     if ccexp[-2:] == "25":
         return True
     else:
         return False
 
-
-# In[86]:
-
-
+#intermediate result
 ecom['CC Exp Date'].apply(exp2025)
 
-
-# In[84]:
-
-
+#the final sum, what the question asks
 sum(ecom['CC Exp Date'].apply(lambda x: exp2025(x)))
 
-
-# In[102]:
-
-
-
-
-
-# ** Hard: What are the top 5 most popular email providers/hosts (e.g. gmail.com, yahoo.com, etc...) **
-
-# In[91]:
-
-
+#most common email domains from data
 def domainGet(email):
     return email.split('@')[-1]
 
-
-# In[100]:
-
-
 ecom['Email'].apply(lambda x: domainGet(x)).value_counts().head(5)
-
-
-# # Great Job!
--- Kaggle salaries/Kaggle salaries.py
+++ Kaggle salaries/Kaggle salaries.py
@@ -1,139 +1,59 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# ___
-# 
-# <a href='http://www.pieriandata.com'> <img src='../../Pierian_Data_Logo.png' /></a>
-# ___
-
-# # SF Salaries Exercise 
-# 
-# Welcome to a quick exercise for you to practice your pandas skills! We will be using the [SF Salaries Dataset](https://www.kaggle.com/kaggle/sf-salaries) from Kaggle! Just follow along and complete the tasks outlined in bold below. The tasks will get harder and harder as you go along.
-
-# ** Import pandas as pd.**
-
-# In[2]:
-
 
 import pandas as pd
 
 
-# ** Read Salaries.csv as a dataframe called sal.**
-
-# In[3]:
-
-
+#import dataframe
 df = pd.read_csv("Salaries.csv")
 
 
-# ** Check the head of the DataFrame. **
-
-# In[4]:
-
-
-df
-
-
-# ** Use the .info() method to find out how many entries there are.**
-
-# In[5]:
-
-
+#check to see what is in df
 df.info()
 
-
-# **What is the average BasePay ?**
-
-# In[6]:
-
-
+#mean of BasePay
 df['BasePay'].mean()
-
-
-# In[7]:
-
-
+#average of BasePay
 df['BasePay'].sum()/df['BasePay'].count()
 
-
-# ** What is the highest amount of OvertimePay in the dataset ? **
-
-# In[8]:
-
-
+#max of OvertimePay
 df['OvertimePay'].max()
 
 
-# ** What is the job title of  JOSEPH DRISCOLL ? Note: Use all caps, otherwise you may get an answer that doesn't match up (there is also a lowercase Joseph Driscoll). **
-
-# In[9]:
-
-
+#Job titel of JOSEPH DRISCOLL
 df[df['EmployeeName']=='JOSEPH DRISCOLL']['JobTitle']
 
 
-# ** How much does JOSEPH DRISCOLL make (including benefits)? **
-
-# In[10]:
-
-
+#TotalPayBenefits of JOSEPH DRISCOLL
 df[df['EmployeeName']=="JOSEPH DRISCOLL"]['TotalPayBenefits']
 
 
 # ** What is the name of highest paid person (including benefits)?**
-
-# In[18]:
-
-
-#df loc ja df[df[]] rivit toimivat molemmat sekä min että max funktioille
+#Rows handling max and min work for both
+#Max TotalPayBenefits
 df.loc[df['TotalPayBenefits'].idxmax()]
 
 
-# ** What is the name of lowest paid person (including benefits)? Do you notice something strange about how much he or she is paid?**
-
-# In[19]:
-
-
+#Min TotalPayBenefits remove comment from end of row to return only name
 df[df['TotalPayBenefits']== df['TotalPayBenefits'].min()] #['EmployeeName']
 
 
-# ** What was the average (mean) BasePay of all employees per year? (2011-2014) ? **
-
-# In[20]:
-
-
+#Mean of BasePay for each year
 df.groupby('Year').mean()['BasePay']
 
 
-# ** How many unique job titles are there? **
-
-# In[21]:
-
-
+#Count of unique job titles
 df['JobTitle'].nunique()
 
-
-# ** What are the top 5 most common jobs? **
-
-# In[22]:
-
-
+#5 most common jobs
 df['JobTitle'].value_counts().head(5)
 
 
-# ** How many Job Titles were represented by only one person in 2013? (e.g. Job Titles with only one occurence in 2013?) **
-
-# In[23]:
-
-
+#sum of job titles with only single entry
 sum(df[df['Year']==2013]['JobTitle'].value_counts() == 1)
 
 
 # ** How many people have the word Chief in their job title? (This is pretty tricky) **
 
-# In[26]:
-
-
+# number of people that have the word chief in their name
 def findChief(text):
     if 'chief' in text.lower().split():
         return True
@@ -141,36 +61,4 @@ def findChief(text):
         return False
 
 
-# In[27]:
-
-
 sum(df['JobTitle'].apply(lambda x: findChief(x)))
-
-
-# In[62]:
-
-
-cdf = df['JobTitle'].apply(lambda x: findChief(x))
-
-
-# In[63]:
-
-
-cdf
-
-
-# ** Bonus: Is there a correlation between length of the Job Title string and Salary? **
-
-# In[64]:
-
-
-df['title_len'] = df['JobTitle'].apply(len)
-
-
-# In[65]:
-
-
-df[['title_len','TotalPayBenefits']].corr() 
-
-
-# # Great Job!
